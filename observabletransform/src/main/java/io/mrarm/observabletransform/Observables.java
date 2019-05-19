63265f14fe284fb85eea5c9802cb674b78b928f7
MCMrARM <mcmrarm@gmail.com>
2019-05-19 18:46:52 +0200

    Object->{Int, Boolean} transforms

--- observabletransform/src/main/java/io/mrarm/observabletransform/Observables.java
+++ observabletransform/src/main/java/io/mrarm/observabletransform/Observables.java
@@ -1,6 +1,7 @@
 package io.mrarm.observabletransform;
 
 import androidx.databinding.ObservableBoolean;
+import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
 
 public class Observables {
@@ -11,11 +12,17 @@ public class Observables {
     public static TransformedObservableBoolean booleanTransform(ObservableBoolean source, BoolBoolTransform transform) {
         return new TransformedObservableBooleanImpl<>(source, (x) -> transform.transform(source.get()));
     }
+    public static <T> TransformedObservableBoolean booleanTransform(ObservableField<T> source, ObjectBoolTransform<T> transform) {
+        return new TransformedObservableBooleanImpl<>(source, (x) -> transform.transform(source.get()));
+    }
 
     public static TransformedObservableInt intTransform(ObservableInt source, IntIntTransform transform) {
         return new TransformedObservableIntImpl<>(source, (x) -> transform.transform(source.get()));
     }
     public static TransformedObservableInt intTransform(ObservableBoolean source, BoolIntTransform transform) {
+        return new TransformedObservableIntImpl<>(source, (x) -> transform.transform(source.get()));
+    }
+    public static <T> TransformedObservableInt intTransform(ObservableField<T> source, ObjectIntTransform<T> transform) {
         return new TransformedObservableIntImpl<>(source, (x) -> transform.transform(source.get()));
     }
 
@@ -26,11 +33,17 @@ public class Observables {
     public interface IntBoolTransform {
         boolean transform(int value);
     }
-    public interface IntIntTransform {
-        int transform(int value);
+    public interface ObjectBoolTransform<T> {
+        boolean transform(T value);
     }
     public interface BoolIntTransform {
         int transform(boolean value);
+    }
+    public interface IntIntTransform {
+        int transform(int value);
+    }
+    public interface ObjectIntTransform<T> {
+        int transform(T value);
     }
 
 }
